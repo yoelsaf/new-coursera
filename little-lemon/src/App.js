f7616ae17d1778d6824c23000d58fc7425523b1a
yoelsaf <yoel.fish28@gmail.com>
2024-05-09 18:10:19 +0300

    some commit message of your choice goes here

---
+++ little-lemon/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Hello react
+      </p>
+    </div>
+  );
+}
+
+export default App;
